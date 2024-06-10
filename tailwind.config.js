4df6d68499fac2ea1ae9fa8536288ab71fb37962
maraver13 <maraverdev@gmail.com>
2024-06-10 10:35:59 +0200

    header and presentation ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js,php}"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'poppins': ['Poppins','sans-serif'],
+        'roboto': ['Roboto','sans-serif'],
+        'roboto-slab': ['Roboto Slab','sans-serif'],
+        'roboto-mono': ['Roboto Mono','monospace'],
+        'roboto-condensed': ['Roboto Condensed','sans-serif'],
+        'roboto-slab-condensed': ['Roboto Slab Condensed','sans-serif'],
+        'roboto-mono-condensed': ['Roboto Mono Condensed','monospace'],
+        'roboto-condensed-condensed': ['Roboto Condensed Condensed','sans-serif'],
+        'roboto-slab-condensed-condensed': ['Roboto Slab Condensed Condensed','sans-serif'],
+        'roboto-mono-condensed-condensed': ['Roboto Mono Condensed Condensed','monospace'],
+        'teko-sans': ['Teko','sans-serif'],
+        'merriweather-regular': ['Merriweather', 'serif'],
+      }
+    },
   },
   plugins: [],
 }
